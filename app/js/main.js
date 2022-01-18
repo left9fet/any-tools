622a49b020fbbef3e7383ce733b621ae79a69e31
Igor Hudakov <left9fet@gmail.com>
2022-01-18 21:23:10 +0500

    done header section

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 $(function() {
 
-    
+    $('.menu__item--dropdown .menu__link').on('click', function(e) {
+        e.preventDefault();
+
+        $('.menu__item--dropdown').toggleClass('menu__item--active')
+        $('.menu__dropdown-list').slideToggle(250)
+    })
 
 });
