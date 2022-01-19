8af9910505e517c3dfe22adda95740089cdada80
Igor Hudakov <left9fet@gmail.com>
2022-01-19 18:31:32 +0500

    done team section

--- app/js/main.js
+++ app/js/main.js
@@ -7,4 +7,10 @@ $(function() {
         $('.menu__dropdown-list').slideToggle(250)
     })
 
+    $('.team__inner').slick({
+        dots: true,
+        prevArrow: '<button class="slick-arrow slick-prev"><svg width="16" height="28" viewBox="0 0 16 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 26L2 14L14 2" stroke="white" stroke-opacity="0.28" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><svg width="16" height="28" viewBox="0 0 16 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 26L14 14L2 2" stroke="white" stroke-opacity="0.28" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+    });
+
 });
