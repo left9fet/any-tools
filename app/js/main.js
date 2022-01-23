a5a9dc7c1a62d2c532e77135a2d5b61963d4d9fb
Igor Hudakov <left9fet@gmail.com>
2022-01-23 22:47:15 +0500

    done team section adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,17 @@ $(function() {
         dots: true,
         prevArrow: '<button class="slick-arrow slick-prev"><svg width="16" height="28" viewBox="0 0 16 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 26L2 14L14 2" stroke="white" stroke-opacity="0.28" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
         nextArrow: '<button class="slick-arrow slick-next"><svg width="16" height="28" viewBox="0 0 16 28" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 26L14 14L2 2" stroke="white" stroke-opacity="0.28" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
+        responsive: [
+            {
+                breakpoint: 998,
+                settings: {
+                    arrows: false,
+                    // centerMode: true,
+                    // centerPadding: '40px',
+                    // slidesToShow: 3
+                }
+            }
+        ]
     });
 
 });
